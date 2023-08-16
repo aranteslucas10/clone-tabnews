819bd21165679e8c284bcdb8e469200d0ff4e422
aranteslucas10 <lucas_arantes10@hotmail.com>
2023-08-16 13:55:36 +0000

    modifica texto h1 no index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Meu nome é Lucas.</h1>
+    return <h1>Se você quiser fazer uma torta de maçã a partir do zero, você deve primeiro inventar o Universo. - Carl Sagan</h1>
 }
 
 export default Home;
