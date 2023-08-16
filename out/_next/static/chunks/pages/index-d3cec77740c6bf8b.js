bbe240b431ea5eaa83a2885b6eacb1b16758ae3f
aranteslucas10 <lucas_arantes10@hotmail.com>
2023-08-16 13:57:08 +0000

    realiza o build estático do projeto

--- out/_next/static/chunks/pages/index-d3cec77740c6bf8b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{5557:function(n,u,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return e(4369)}])},4369:function(n,u,e){"use strict";e.r(u);var t=e(5893);u.default=function(){return(0,t.jsx)("h1",{children:"Meu nome \xe9 Lucas."})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=5557)}),_N_E=n.O()}]);
