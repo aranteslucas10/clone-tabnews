7367db0fe30be66930b4639c3eb0a3549bb71fab
aranteslucas10 <lucas_arantes10@hotmail.com>
2023-08-24 02:57:57 +0000

    adiciona uma maçanzinha lindinha 😍

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 function Home() {
   return (
-    <h1>
-      Se você quiser fazer uma torta de maçã a partir do zero, você deve
-      primeiro inventar o Universo. - Carl Sagan
-    </h1>
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        textAlign: "center",
+        backgroundColor: "#fff",
+        margin: 0,
+        padding: 0,
+      }}
+    >
+      <div style={{ padding: "100px" }}>
+        <div style={{ fontSize: "24px", color: "#333", marginBottom: "20px" }}>
+          "Se você quiser fazer uma torta de maçã a partir do zero, você deve
+          primeiro inventar o Universo."
+        </div>
+        <div style={{ fontSize: "18px", color: "#777" }}>- Carl Sagan</div>
+      </div>
+      <img src="./apple-81.gif" style={{ maxWidth: "25%", heigth: "auto" }} />
+    </div>
   );
 }
 
