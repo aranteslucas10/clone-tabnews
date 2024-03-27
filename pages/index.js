671c54a80547c3074f21d1a5df98cb1a6ebb9822
Lucas Arantes <lucas_arantes10@hotmail.com>
2024-03-26 22:08:21 -0300

    oi cris

--- pages/index.js
+++ pages/index.js
@@ -9,14 +9,20 @@ function Home() {
         padding: 0,
       }}
     >
-      <div style={{ padding: "100px" }}>
+      {/* <div style={{ padding: "100px" }}>
         <div style={{ fontSize: "24px", color: "#333", marginBottom: "20px" }}>
           "Se você quiser fazer uma torta de maçã a partir do zero, você deve
           primeiro inventar o Universo."
         </div>
         <div style={{ fontSize: "18px", color: "#777" }}>- Carl Sagan</div>
       </div>
-      <img src="./apple-81.gif" style={{ maxWidth: "25%", heigth: "auto" }} />
+      <img src="./apple-81.gif" style={{ maxWidth: "25%", heigth: "auto" }} /> */}
+      <div style={{ padding: "100px" }}>
+        <div style={{ fontSize: "24px", color: "#333", marginBottom: "20px" }}>
+          "Oie Cris! Saudades! ❤️ Se você gosta de mim dá uma risadinha! 😝"
+        </div>
+        <div style={{ fontSize: "18px", color: "#777" }}>- Lucas</div>
+      </div>
     </div>
   );
 }
