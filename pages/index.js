7c0cdd43ad8599eec7f4978e0eb693a52bcdc78f
Lucas Arantes <lucas_arantes10@hotmail.com>
2024-05-23 08:28:17 -0300

    atualiza index.js

--- pages/index.js
+++ pages/index.js
@@ -17,12 +17,6 @@ function Home() {
         <div style={{ fontSize: "18px", color: "#777" }}>- Carl Sagan</div>
       </div>
       <img src="./apple-81.gif" style={{ maxWidth: "25%", heigth: "auto" }} />
-      {/* <div style={{ padding: "100px" }}>
-        <div style={{ fontSize: "24px", color: "#333", marginBottom: "20px" }}>
-          "Oie Cris! Saudades! ❤️ Se você gosta de mim dá uma risadinha! 😝"
-        </div>
-        <div style={{ fontSize: "18px", color: "#777" }}>- Lucas</div>
-      </div> */}
     </div>
   );
 }
