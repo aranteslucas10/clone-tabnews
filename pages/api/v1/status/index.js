6178a60c111f50570e11cd3944345274771f5476
Lucas Arantes <lucas_arantes10@hotmail.com>
2025-01-06 09:21:25 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,17 +1,29 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: await database.getDatabaseVersion(),
-        max_connections: await database.getMaxConnections(),
-        opened_connections: await database.getUsedConnections(),
+  try {
+    const updatedAt = new Date().toISOString();
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: await database.getDatabaseVersion(),
+          max_connections: await database.getMaxConnections(),
+          opened_connections: await database.getUsedConnections(),
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.log("\n Erro dentro do catch do controller:");
+    console.error(publicErrorObject);
+
+    response.status(500).json(publicErrorObject);
+  }
 }
 
 export default status;
