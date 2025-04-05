b47a0aaad144d066ca088718ef6a6eb8cabcec9b
Lucas Arantes <lucas_arantes10@hotmail.com>
2025-04-05 14:21:45 -0300

    feat: add migration to create `users` table

---
+++ infra/migrations/1743871272649_users.js
@@ -0,0 +1,46 @@
+exports.up = (pgm) => {
+    pgm.createTable("users", {
+        id: {
+            type: "uuid",
+            primaryKey: true,
+            default: pgm.func("gen_random_uuid()")
+        },
+
+        // For reference, GitHub limits usernames to 39 characters.
+        username: {
+            type: "varchar(30)",
+            notNull: true,
+            unique: true,
+            notEmpty: true
+        },
+
+        // Why 254 in length? https://stackoverflow.com/a/1199238
+        email: {
+            type: "varchar(254)",
+            notNull: true,
+            unique: true,
+            notEmpty: true
+        },
+
+        // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
+        password: {
+            type: "varchar(72)",
+            notNull: true,
+            notEmpty: true
+        },
+        
+        created_at: {
+            type: "timestamptz",
+            notNull: true,
+            default: pgm.func("timezone('UTC', now())")
+        },
+
+        updated_at: {
+            type: "timestamptz",
+            notNull: true,
+            default: pgm.func("timezone('UTC', now())"),
+        }
+    });
+};
+
+exports.down = false;
